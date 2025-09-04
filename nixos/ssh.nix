19a76c92e93396bc8be741d57d21d5454f8d9739
furilo <53122773+furilofox@users.noreply.github.com>
2025-09-04 22:05:57 +0200

    SSH Changes

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -1,10 +1,63 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   services.openssh = {
     enable = true;
     settings = {
       KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
+      PermitRootLogin = "yes";
       AllowUsers = [config.var.username];
+      PasswordAuthentication = false;
+      PubkeyAuthentication = true;
     };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+  };
+
+  programs.ssh = {
+    startAgent = true;
+    extraConfig = ''
+      Host pandora
+        HostName 192.168.20.21
+        User ${config.var.username}
+        ForwardAgent yes
+        
+      Host athenas
+        HostName 192.168.20.22
+        User ${config.var.username}
+        ForwardAgent yes
+        
+      Host promethea
+        HostName 192.168.20.22
+        User ${config.var.username}
+        ForwardAgent yes
+        
+      Host *
+        AddKeysToAgent yes
+        IdentityFile ~/.ssh/id_ed25519
+    '';
+    knownHosts = {
+      pandora = {
+        hostNames = ["pandora" "192.168.20.21"];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3akVuki5R7QbCLl5+l9l0IpxapzEOJ6acAwQBwxYcm fabian@pandora";
+      };
+      athenas = {
+        hostNames = ["athenas" "192.168.20.22"];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9feviQt7FkGvXKpWic+G+9q8wzTFzwIM/6EowDeTxi fabian@promethea";
+      };
+      promethea = {
+        hostNames = ["promethea" "192.168.20.22"];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9feviQt7FkGvXKpWic+G+9q8wzTFzwIM/6EowDeTxi fabian@promethea";
+      };
+    };
+  };
+
+  users.users.${config.var.username} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3akVuki5R7QbCLl5+l9l0IpxapzEOJ6acAwQBwxYcm fabian@pandora"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9feviQt7FkGvXKpWic+G+9q8wzTFzwIM/6EowDeTxi fabian@promethea"
+    ];
   };
 }
